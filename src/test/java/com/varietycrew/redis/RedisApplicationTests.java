04ae382eaac5af81037bc119fb1a23fade76681a
Rohdamin <rohdamin3@gmail.com>
2024-10-04 20:26:12 +0900

    init

---
+++ src/test/java/com/varietycrew/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varietycrew.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
